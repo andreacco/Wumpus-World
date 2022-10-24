b1b304a0a4b9eee32d29d24aa5c05514a3080746
Andrea Carvajal Osorio <andreacc9199@gmail.com>
2022-10-24 01:03:22 -0400

    first commit

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  restart() {
+    console.log("you restarted the game")
+      // wumpusWorld = new World(roomsPerRow);
+      // flies_sound.stop();
+      // wind_sound.forEach(sound => {
+      //     sound.stop();
+      // });
+      // victory_sound.stop();
+      // defeat_sound.stop();
+  }
+
+}
